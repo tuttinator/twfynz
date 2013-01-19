4d4f728cfd2e104637dfe07bb7c08cb116046e0f
Caleb <contact@calebtutty.com>
2013-01-20 11:38:12 +1300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twfynz::Application.config.secret_token = '58dfe41f5ba4444a81da4ec0882a9ef480a5dc7ad393c9abf368dc31a1dd606e06dd03825a6210ac1a6db6e1dbfeb187bb3f465a545135e679c6864744741380'
