b6b141fc604c6dff7eba726ca5f74ea20ba961b3
Rowan Crawford <wombleton@gmail.com>
2013-10-10 16:34:52 +1300

    Obliterate old rails with new; pull things back over once working again

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twfynz::Application.config.secret_token = 'c529b62e984f68b75789e1d72ae127eef958ef22d726069ddc8d071bd411c85ebb1ee4b279d47e7745e1cc00cc5c25b2d30701bee0c5386e296b6c85d65fb660'
