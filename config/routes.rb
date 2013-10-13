6e23dbd76af056d88dcdb42d7b91d2b926d380ba
Rowan Crawford <wombleton@gmail.com>
2013-10-14 07:48:08 +1300

    Add many routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Twfynz::Application.routes.draw do
   root :to => "application#home"
+  match "search" => "application#search"
+  match "about" => "application#about"
+
+  resources :bills
+  resources :portfolios
+  resources :mps
+  resources :organisations
+  resources :parliaments
+  resources :debates
+  resources :parties
 end
