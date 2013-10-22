4677c98f581b0a35078fac8100ba64c86717ed33
Rowan Crawford <wombleton@gmail.com>
2013-10-23 12:08:45 +1300

    Create committee if not found

--- app/models/committee_chair.rb
+++ app/models/committee_chair.rb
@@ -5,7 +5,18 @@ class CommitteeChair < ActiveRecord::Base
   has_many :oral_answers, :as => :answer_from
 
   def self.from_name name
-    CommitteeChair.find_by_role name
+    chair = CommitteeChair.find_by_role(name)
+
+    if chair.nil?
+      committee = Committee.from_name(name.gsub('Chairperson of the', ''))
+      if committee
+        CommitteeChair.create(:role => name, :committee => committee)
+      else
+        nil
+      end
+    else
+      chair
+    end
   end
 
   def title
