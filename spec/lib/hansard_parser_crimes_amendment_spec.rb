76bb2b018678efed59dd4101183e8e884a89ef51
Rowan Crawford <wombleton@gmail.com>
2014-01-20 11:01:43 +1300

    Encoding, get a parliament

--- spec/lib/hansard_parser_crimes_amendment_spec.rb
+++ spec/lib/hansard_parser_crimes_amendment_spec.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 require File.dirname(__FILE__) + '/hansard_parser_spec_helper'
 
 describe HansardParser, "when passed Crimes (Substituted Section 59) Amendment Bill, Third Reading" do
@@ -16,7 +17,8 @@ describe HansardParser, "when passed Crimes (Substituted Section 59) Amendment B
     @file_name = 'nil'
 
     def_parties
-    HansardParser.stub!(:load_file).and_return html
+    Parliament.stub(:latest).and_return double(Parliament)
+    HansardParser.stub(:load_file).and_return html
     @debate = parse_debate
     @sub_debate = @debate.sub_debate
     count = @sub_debate.contributions.size
