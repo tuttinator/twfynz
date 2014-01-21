cb1e6fa1cada1784b34c74f423dbc125e9e7b49a
Rowan Crawford <wombleton@gmail.com>
2014-01-21 14:03:08 +1300

    Update to latest rspec

--- spec/lib/hansard_parser_debate_heading_is_h1_spec.rb
+++ spec/lib/hansard_parser_debate_heading_is_h1_spec.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 require File.dirname(__FILE__) + '/hansard_parser_spec_helper'
 
 describe HansardParser, "when passed Crimes (Substituted Section 59) Amendment Bill, Third Reading" do
@@ -16,7 +17,7 @@ describe HansardParser, "when passed Crimes (Substituted Section 59) Amendment B
     @file_name = 'nil'
 
     def_parties
-    HansardParser.stub!(:load_file).and_return html
+    HansardParser.stub(:load_file).and_return html
     @debate = parse_debate
     @sub_debate = @debate.sub_debate
   end
