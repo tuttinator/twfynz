a3c82276e7ad630200942e85f41e68564e4f61a3
Caleb Tutty <caleb@prettymint.co.nz>
2014-03-31 22:56:22 +1300

    Clean up cruft and add Rails 4 default config.i18n.enforce_available_locales = true to application.rb

--- config/application.rb
+++ config/application.rb
@@ -38,6 +38,8 @@ module Twfynz
 
     config.autoload_paths += ["#{config.root}/lib"]
 
+    config.i18n.enforce_available_locales = true
+
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
   end
